2a56ac36428affdd4d60f3d246e426c172043122
carpemc <carpemc@gmail.com>
2018-10-28 20:50:13 -0600

    semi working app

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+var friendsArray = [
+	{
+		name: "Ron",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Leslie",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Andy",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Jerry",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }, {
+		name: "Tom",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "April",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }, {
+		name: "Ben",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Chris",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Ann",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }, {
+		name: "Jean",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    }
+];
+
+
+module.exports = friendsArray;
