e58b22c84a56daf02b6fb1b7f6bddf4962f4e5b3
mcarpent87 <carpemc@gmail.com>
2018-10-30 00:30:22 -0600

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,42 +1,46 @@
 var friendsArray = [
 	{
-		name: "Ron",
+		name: "Ron Swanson",
 		photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/2016_RonSwanson_Press_210716-1-920x610.jpg",
 		scores: [4, 5, 3, 1, 2, 5, 3, 3, 5, 2]
     }, {
-		name: "Leslie",
+		name: "Leslie Knope",
 		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/38/Leslie.png/revision/latest?cb=20111015205925",
 		scores: [1, 1, 5, 2, 2, 2, 3, 1, 4, 2]
     }, {
-		name: "Andy",
+		name: "Andy Dwyer",
 		photo: "https://imgix.bustle.com/rehost/2016/9/13/90111dc4-0e9b-45ae-8f2e-42199f9eb437.jpg?w=970&h=582&fit=crop&crop=faces&auto=format&q=70",
 		scores: [1, 4, 2, 5, 3, 2, 3, 5, 5, 2]
     }, {
-		name: "Jerry",
+		name: "Jerry Gurgich",
 		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/38/Jerry.jpg/revision/latest?cb=20110809181649",
 		scores: [1, 1, 4, 2, 1, 1, 1, 1, 1, 1]
     }, {
-		name: "Tom",
-		photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwip9bWulK3eAhXGz4MKHew0Dq0QjRx6BAgBEAU&url=https%3A%2F%2Fjunkee.com%2Fsadly-aziz-ansari-is-nothing-like-tom-haverford-in-real-life%2F21949&psig=AOvVaw38YPPOaYl12PL_xHcqtNFE&ust=1540954328745224",
+		name: "Tom Haverford",
+		photo: "https://wh1k8zidop.inscname.net/big/253798.jpg?v=1462392244",
 		scores: [1, 1, 2, 3, 1, 2, 5, 5, 5, 5]
     }, {
-		name: "April",
+		name: "April Ludgate",
 		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ed/April-0.png/revision/latest?cb=20180116070637",
 		scores: [5, 1, 3, 1, 4, 1, 3, 1, 2, 2]
     }, {
-		name: "Ben",
+		name: "Ben Wyatt",
 		photo: "https://imagesvc.timeincapp.com/v3/fan/image?url=https://culturess.com/wp-content/blogs.dir/386/files/2017/03/635928161632591870-426328109_ddddddd.jpg&c=sc&w=1916&h=1078",
 		scores: [2, 2, 5, 2, 3, 2, 4, 2, 1, 1]
     }, {
-		name: "Chris",
+		name: "Chris Traeger",
 		photo: "https://tv-fanatic-res.cloudinary.com/iu/s--FpZc1zl_--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1371220245/chris-traeger-photograph.png",
 		scores: [1, 1, 4, 5, 2, 2, 3, 3, 4, 3]
     }, {
-		name: "Ann",
+		name: "Ann Perkins",
 		photo: "https://i.pinimg.com/originals/47/3c/bf/473cbff9f88947a5b2c4766bf486c73a.jpg",
 		scores: [3, 1, 2, 3, 1, 2, 5, 1, 1, 1]
     }, {
-		name: "Donna",
+		name: "Lil' Sebastian",
+		photo: "https://i.pinimg.com/originals/59/00/77/590077ff655ac5ca6ca05f1a25cd6a7c.jpg",
+		scores: [1, 1, 1, 1, 1, 5, 1, 5, 4, 4]
+    }, {
+		name: "Donna Meagle",
 		photo: "https://pbs.twimg.com/profile_images/661295683366989824/Hf6UD-gC_400x400.jpg",
 		scores: [3, 1, 3, 3, 1, 2, 5, 1, 1, 5]
 		}
